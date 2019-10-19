c8f85d2ba8fd5ae7b35aab87a31b33e1808b1ff7
Ioram Gordadze <Yoh0xFF@gmail.com>
2019-10-19 16:43:24 +0400

    First commit

---
+++ src/main/java/io/example/BasicApi.java
@@ -0,0 +1,122 @@
+package io.example;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BasicApi {
+
+    public static void main(String[] args) {
+        new BasicApi().run();
+    }
+
+    public void run() {
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf sparkConf = new SparkConf()
+                .setAppName("StartingSpark")
+                .setMaster("local[*]");
+
+        try (JavaSparkContext sparkContext = new JavaSparkContext(sparkConf)) {
+            reduce(sparkContext);
+
+            map(sparkContext);
+
+            count(sparkContext);
+
+            tuples(sparkContext);
+
+            pairRdd(sparkContext);
+
+            flatMap(sparkContext);
+
+            filter(sparkContext);
+        }
+    }
+
+    private void filter(JavaSparkContext sparkContext) {
+        System.out.println("\n\n---------------> filter");
+        List<String> input = Arrays.asList(
+                "WARN: Tuesday 4 September 0405",
+                "ERROR: Tuesday 4 September 0408",
+                "FATAL: Wednesday 5 September 1632",
+                "ERROR: Friday 7 September 1854",
+                "WARN: Saturday 8 September 1942");
+
+        sparkContext.parallelize(input)
+                .flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator())
+                .filter(word -> word.length() > 1)
+                .foreach(word -> System.out.println(word));
+    }
+
+    private void flatMap(JavaSparkContext sparkContext) {
+        System.out.println("\n\n---------------> flatMap");
+        List<String> input = Arrays.asList(
+                "WARN: Tuesday 4 September 0405",
+                "ERROR: Tuesday 4 September 0408",
+                "FATAL: Wednesday 5 September 1632",
+                "ERROR: Friday 7 September 1854",
+                "WARN: Saturday 8 September 1942");
+
+        sparkContext.parallelize(input)
+                .flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator())
+                .foreach(word -> System.out.println(word));
+    }
+
+    private void pairRdd(JavaSparkContext sparkContext) {
+        System.out.println("\n\n---------------> pairRdd");
+        List<String> input = Arrays.asList(
+                "WARN: Tuesday 4 September 0405",
+                "ERROR: Tuesday 4 September 0408",
+                "FATAL: Wednesday 5 September 1632",
+                "ERROR: Friday 7 September 1854",
+                "WARN: Saturday 8 September 1942");
+
+        sparkContext.parallelize(input)
+                .mapToPair(x -> new Tuple2<>(x.split(":")[0].trim(), 1L))
+                .reduceByKey((x, y) -> x + y)
+                .foreach(tuple -> System.out.println(tuple._1 + ": " + tuple._2));
+    }
+
+    private void tuples(JavaSparkContext sparkContext) {
+        System.out.println("\n\n---------------> tuples");
+        List<Integer> input = Arrays.asList(35, 12, 90, 20, 25);
+
+        JavaRDD<Integer> rdd = sparkContext.parallelize(input);
+        JavaRDD<Tuple2<Integer, Double>> sqrtRdd = rdd.map(x -> new Tuple2<>(x, Math.sqrt(x)));
+        sqrtRdd.foreach(x -> System.out.println(x));
+    }
+
+    private void count(JavaSparkContext sparkContext) {
+        System.out.println("\n\n---------------> count");
+        List<Integer> input = Arrays.asList(35, 12, 90, 20, 25);
+
+        JavaRDD<Integer> rdd = sparkContext.parallelize(input);
+        System.out.println(rdd.count());
+        System.out.println(rdd.map(x -> 1).reduce((x, y) -> x + y));
+    }
+
+    private void map(JavaSparkContext sparkContext) {
+        System.out.println("\n\n---------------> mapSqrt");
+        List<Integer> input = Arrays.asList(35, 12, 90, 20, 25);
+
+        JavaRDD<Integer> rdd = sparkContext.parallelize(input);
+        JavaRDD<Double> sqrtRdd = rdd.map(x -> Math.sqrt(x));
+        sqrtRdd.foreach(x -> System.out.println(x));
+    }
+
+    private void reduce(JavaSparkContext sparkContext) {
+        System.out.println("\n\n---------------> sumByReduce");
+        List<Integer> input = Arrays.asList(35, 12, 90, 20, 25);
+
+        JavaRDD<Integer> rdd = sparkContext.parallelize(input);
+        int sum = rdd.reduce((x, y) -> x + y);
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/io/example/KeywordRanking.java
@@ -0,0 +1,76 @@
+package io.example;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class KeywordRanking {
+
+    public static void main(String[] args) {
+        new KeywordRanking().run();
+    }
+
+    public void run() {
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        SparkConf sparkConf = new SparkConf()
+                .setAppName("StartingSpark")
+                .setMaster("local[*]");
+
+        try (JavaSparkContext sparkContext = new JavaSparkContext(sparkConf)) {
+            keywordRanking(sparkContext);
+        }
+    }
+
+    public void keywordRanking(JavaSparkContext sparkContext) {
+        JavaRDD<String> initialRDD = sparkContext
+                .textFile("src/main/resources/io/example/subtitles/input-spring.txt");
+
+        JavaPairRDD<Long, String> result = initialRDD
+                .map(sentence -> sentence.replaceAll("[^a-zA-Z\\s]", "").trim().toLowerCase()) // leave only letters
+                .filter(sentence -> StringUtils.isNotBlank(sentence)) // filter blank lines
+                .flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator()) // explode sentence into words
+                .filter(word -> StringUtils.isNotBlank(word) && Util.isNotBoring(word)) // filter empty and boring words
+                .mapToPair(word -> new Tuple2<>(word, 1L)) // make pair rdd
+                .reduceByKey((x, y) -> x + y) // count words
+                .mapToPair(tuple -> new Tuple2<>(tuple._2, tuple._1)) // switch key and value places
+                .sortByKey(false); // sort result by key
+
+        result.take(50).forEach(System.out::println);
+    }
+}
+
+class Util {
+
+    private static final Set<String> boringWords = new HashSet<>();
+
+    static {
+        try (InputStream is = Util.class.getResourceAsStream("subtitles/boring-words.txt")) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            br.lines().forEach(boringWords::add);
+        } catch (IOException ex) {
+            throw new AssertionError(ex);
+        }
+    }
+
+    public static boolean isBoring(String word) {
+        return boringWords.contains(word);
+    }
+
+    public static boolean isNotBoring(String word) {
+        return !isBoring(word);
+    }
+}
